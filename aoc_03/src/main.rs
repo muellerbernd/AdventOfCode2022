1bcbf8d3985e6157d6a0701d106ea37ab10f2fe7
Bernd Müller <bernd@muellerbernd.de>
2022-12-03 21:07:05 +0100

    aoc03 task 2 done

--- aoc_03/src/main.rs
+++ aoc_03/src/main.rs
@@ -26,4 +26,34 @@ fn main() {
         }
     }
     println!("task 1: {}", prios.iter().sum::<u32>());
+
+    let mut task2_prios: Vec<u32> = Vec::new();
+    let lines: Vec<String> = contents.lines().map(|x| x.to_string()).collect();
+    for i in (0..lines.len() - 2).step_by(3) {
+        let elf1 = lines.get(i).unwrap();
+        let elf2 = lines.get(i + 1).unwrap();
+        let elf3 = lines.get(i + 2).unwrap();
+        for v in b'a'..=b'z' {
+            let val: char = v as char;
+            if elf1.contains(&val.to_string())
+                && elf2.contains(&val.to_string())
+                && elf3.contains(&val.to_string())
+            {
+                task2_prios.push(val as u32 - 96);
+                break;
+            }
+        }
+
+        for v in b'A'..=b'Z' {
+            let val: char = v as char;
+            if elf1.contains(&val.to_string())
+                && elf2.contains(&val.to_string())
+                && elf3.contains(&val.to_string())
+            {
+                task2_prios.push(val as u32 - 64 + 26);
+                break;
+            }
+        }
+    }
+    println!("task 2: {}", task2_prios.iter().sum::<u32>());
 }
