8e4f4d67aff19a84032977021b8ea991b556077e
Bernd Müller <bernd@muellerbernd.de>
2022-12-06 19:41:21 +0100

    fix typo

--- aoc_06/src/main.rs
+++ aoc_06/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 fn main() {
-    let file_path = "../inputs/aoc_06.txt";
-    // let file_path = "test_input.txt";
+    // let file_path = "../inputs/aoc_06.txt";
+    let file_path = "test_input.txt";
 
     let raw_input: String = read_to_string(file_path)
         .expect("Should have been able to read the file")
@@ -9,17 +9,17 @@ fn main() {
     println!("{}", raw_input);
     // let window_size: usize = 4;
     let window_size: usize = 14; // task2
-    for (i, _) in raw_input.chars().enumerate() {
-        if i >= window_size-1 {
-            let slice: String = raw_input[i - (window_size-1)..=i].to_string().to_owned();
-            println!("{:?}", slice);
-            let dublicates: Vec<char> = slice.chars().filter(|c| slice.matches(&c.to_string()).count() > 1).collect();
-            println!("{:?}", dublicates);
-            if dublicates.is_empty()
-            {
-                println!("first marker after character {:?}", i+1);
-                break;
-            }
+    for i in window_size - 1..raw_input.len() {
+        let slice: String = raw_input[i - (window_size - 1)..=i].to_string().to_owned();
+        println!("{:?}", slice);
+        let duplicates: Vec<char> = slice
+            .chars()
+            .filter(|c| slice.matches(&c.to_string()).count() > 1)
+            .collect();
+        println!("{:?}", duplicates);
+        if duplicates.is_empty() {
+            println!("first marker after character {:?}", i + 1);
+            break;
         }
     }
 }
