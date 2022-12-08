acf6c80d09b4287635aa0366c1a813bd7276cc90
Bernd Müller <github@muellerbernd.de>
2022-12-08 23:53:07 +0100

    day8 task2 done

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -36,6 +36,28 @@ fn get_visible_trees(
         }
     }
 }
+
+fn get_scenic_scores(
+    forest: &Vec<Vec<u32>>,
+    scenic_scores: &mut Vec<Vec<i32>>,
+    check_dim1: Vec<usize>,
+    check_dim2: Vec<usize>,
+) {
+    for y in check_dim1 {
+        let mut max_view_dists: Vec<i32> = vec![0; 10];
+        for x in &check_dim2 {
+            let tree: usize = forest[y][*x] as usize;
+            scenic_scores[y][*x] *= max_view_dists[tree];
+            max_view_dists = max_view_dists.iter().map(|x| x + 1).collect();
+            max_view_dists = max_view_dists
+                .iter()
+                .enumerate()
+                .map(|(i, v)| if i <= tree { 1 } else { *v })
+                .collect();
+        }
+    }
+}
+
 fn main() {
     let file_path = "../inputs/aoc_08.txt";
     // let file_path = "test_input.txt";
@@ -46,6 +68,7 @@ fn main() {
         .lines()
         .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
         .collect();
+    println!("{:?}", forest);
     let mut visible_trees: Vec<Vec<bool>> = raw_input
         .lines()
         .map(|l| l.chars().map(|_| false).collect())
@@ -82,4 +105,45 @@ fn main() {
         .map(|r| r.iter().filter(|c| **c).count())
         .sum();
     println!("task1_res {:?}", task1_res);
+
+    let mut forest_task2: Vec<Vec<u32>> = raw_input
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect();
+    let mut scenic_scores: Vec<Vec<i32>> = raw_input
+        .lines()
+        .map(|l| l.chars().map(|_| 1).collect())
+        .collect();
+    get_scenic_scores(
+        &forest_task2,
+        &mut scenic_scores,
+        (0..forest_task2.len()).collect(),
+        (0..forest_task2[0].len()).collect(),
+    );
+    get_scenic_scores(
+        &forest_task2,
+        &mut scenic_scores,
+        (0..forest_task2.len()).collect(),
+        (0..forest_task2[0].len()).rev().collect(),
+    );
+    forest_task2 = transpose(forest_task2);
+    scenic_scores = transpose(scenic_scores);
+    get_scenic_scores(
+        &forest_task2,
+        &mut scenic_scores,
+        (0..forest_task2.len()).collect(),
+        (0..forest_task2[0].len()).collect(),
+    );
+    get_scenic_scores(
+        &forest_task2,
+        &mut scenic_scores,
+        (0..forest_task2.len()).collect(),
+        (0..forest_task2[0].len()).rev().collect(),
+    );
+    println!("scenic_scores {:?}", scenic_scores);
+    let task2_res = scenic_scores
+        .iter()
+        .map(|r| r.iter().max().unwrap())
+        .max().unwrap();
+    println!("task2_res {:?}", task2_res);
 }
