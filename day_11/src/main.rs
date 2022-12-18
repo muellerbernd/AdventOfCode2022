9a1507dc601e88b179da5119999fa02cba07b30a
Bernd Müller <github@muellerbernd.de>
2022-12-18 15:58:16 +0100

    started day 11

---
+++ day_11/src/main.rs
@@ -0,0 +1,10 @@
+use std::fs::read_to_string;
+
+fn main() {
+    // let file_path = "../inputs/aoc_11.txt";
+    let file_path = "test_input.txt";
+
+    let raw_input: String =
+        read_to_string(file_path).expect("Should have been able to read the file");
+    println!("{}", raw_input);
+}
