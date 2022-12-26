4c74ac4dc30801a31cfe44d5db22b93a41efc7d5
Bernd Müller <github@muellerbernd.de>
2022-12-26 20:38:40 +0100

    day 12: build graph

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,87 +1,69 @@
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
-fn get_graph(height_map: &Vec<Vec<i32>>) {
-    let mut graph: HashMap<(usize, usize), Vec<(usize, usize)>> = HashMap::new();
-    for y_index in 0..height_map.len() {
-        for x_index in 0..height_map[0].len() - 1 {
-            let val_l = height_map[y_index][x_index];
-            let val_r = height_map[y_index][x_index + 1];
-            if val_l < val_r {
-                graph
-                    .entry((x_index + 1, y_index))
-                    .and_modify(|v| v.push((x_index, y_index)))
-                    .or_insert(vec![(x_index, y_index)]);
-                if val_r - val_l <= 1 {
-                    graph
-                        .entry((x_index, y_index))
-                        .and_modify(|v| v.push((x_index + 1, y_index)))
-                        .or_insert(vec![(x_index + 1, y_index)]);
-                }
-            } else if val_l == val_r {
-                graph
-                    .entry((x_index + 1, y_index))
-                    .and_modify(|v| v.push((x_index, y_index)))
-                    .or_insert(vec![(x_index, y_index)]);
-                graph
-                    .entry((x_index, y_index))
-                    .and_modify(|v| v.push((x_index + 1, y_index)))
-                    .or_insert(vec![(x_index + 1, y_index)]);
-            } else {
-                graph
-                    .entry((x_index, y_index))
-                    .and_modify(|v| v.push((x_index + 1, y_index)))
-                    .or_insert(vec![(x_index + 1, y_index)]);
-                if val_l - val_r <= 1 {
-                    graph
-                        .entry((x_index + 1, y_index))
-                        .and_modify(|v| v.push((x_index, y_index)))
-                        .or_insert(vec![(x_index, y_index)]);
-                }
-            }
-        }
+fn print_2d(v: &Vec<Vec<i32>>) {
+    for l in v {
+        println!("{:?}", l);
     }
-    for x_index in 0..height_map[0].len() {
-        for y_index in 0..height_map.len() - 1 {
-            let val_u = height_map[y_index][x_index];
-            let val_d = height_map[y_index + 1][x_index];
-            if val_u < val_d {
-                graph
-                    .entry((x_index, y_index))
-                    .and_modify(|v| v.push((x_index, y_index + 1)))
-                    .or_insert(vec![(x_index, y_index + 1)]);
-                if val_u - val_d <= 1 {
-                    graph
-                        .entry((x_index, y_index + 1))
-                        .and_modify(|v| v.push((x_index, y_index)))
-                        .or_insert(vec![(x_index, y_index)]);
-                }
-            } else if val_u == val_d {
-                graph
-                    .entry((x_index, y_index))
-                    .and_modify(|v| v.push((x_index, y_index + 1)))
-                    .or_insert(vec![(x_index, y_index + 1)]);
-                graph
-                    .entry((x_index, y_index + 1))
-                    .and_modify(|v| v.push((x_index, y_index)))
-                    .or_insert(vec![(x_index, y_index)]);
+}
+
+fn setup_graph(height_map: &Vec<Vec<i32>>) {
+    let mut graph: HashMap<(usize, usize), Vec<(usize, usize)>> = HashMap::new();
+    let y_len = height_map.len();
+    let x_len = height_map[0].len();
+    for y_index in 0..y_len {
+        for x_index in 0..x_len {
+            let curr_val = height_map[y_index][x_index];
+            let val_l = if x_index >= 1 {
+                Some(height_map[y_index][x_index - 1])
             } else {
+                None
+            };
+            let val_r = if x_index < x_len - 1 {
+                Some(height_map[y_index][x_index + 1])
+            } else {
+                None
+            };
+            let val_u = if y_index >= 1 {
+                Some(height_map[y_index - 1][x_index])
+            } else {
+                None
+            };
+            let val_d = if y_index < y_len - 1 {
+                Some(height_map[y_index + 1][x_index])
+            } else {
+                None
+            };
+
+            if val_r.is_some() && (val_r.unwrap() - curr_val).abs() <= 1 {
                 graph
                     .entry((x_index, y_index))
                     .and_modify(|v| v.push((x_index + 1, y_index)))
                     .or_insert(vec![(x_index + 1, y_index)]);
-                if val_u - val_d <= 1 {
-                    graph
-                        .entry((x_index, y_index))
-                        .and_modify(|v| v.push((x_index, y_index + 1)))
-                        .or_insert(vec![(x_index, y_index + 1)]);
-                }
+            }
+            if val_l.is_some() && (val_l.unwrap() - curr_val).abs() <= 1 {
+                graph
+                    .entry((x_index, y_index))
+                    .and_modify(|v| v.push((x_index - 1, y_index)))
+                    .or_insert(vec![(x_index - 1, y_index)]);
+            }
+            if val_u.is_some() && (val_u.unwrap() - curr_val).abs() <= 1 {
+                graph
+                    .entry((x_index, y_index))
+                    .and_modify(|v| v.push((x_index, y_index - 1)))
+                    .or_insert(vec![(x_index, y_index - 1)]);
+            }
+            if val_d.is_some() && (val_d.unwrap() - curr_val).abs() <= 1 {
+                graph
+                    .entry((x_index, y_index))
+                    .and_modify(|v| v.push((x_index, y_index + 1)))
+                    .or_insert(vec![(x_index, y_index + 1)]);
             }
         }
     }
     println!("{:?}", graph);
-    println!("{:?}", graph.get(&(4,2)));
-    println!("{:?}", graph.get(&(1,1)));
+    println!("{:?}", graph.get(&(4, 2)));
+    println!("{:?}", graph.get(&(1, 1)));
 }
 
 fn main() {
@@ -116,6 +98,7 @@ fn main() {
         .iter()
         .map(|l| l.chars().map(|c| c as i32 - 'a' as i32).collect())
         .collect();
-    println!("{:?} {:?} {:?}", height_map, start_pos, goal_pos);
-    get_graph(&height_map);
+    print_2d(&height_map);
+    // get_graph(&height_map);
+    setup_graph(&height_map);
 }
